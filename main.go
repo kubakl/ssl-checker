9ea0110bd8ae3bc8c3c1f66f4180f52f2d8bf07e
Jakub Klimek <kuba@edp.pl>
2021-06-09 19:13:53 +0200

    Add basic functionality

--- main.go
+++ main.go
@@ -3,21 +3,56 @@ package main
 import (
   "fmt"
   "flag"
-  _ "os"
+  "os"
+  "time"
+  "crypto/tls"  
+  "errors"
 )
 
 var (
   email = flag.String("e", "", "Email address for sending expiry date of provided domains")
-  domain = flag.String("d", "", "Single domain to be checked, in case you don't have a file with domain names")
+  domain = flag.String("d", "", "Single domain to be checked, in case you don't have a file containing domain names")
   filename = flag.String("f", "", "Route to a file with domains to check")
+  left = flag.Bool("l", false, "Is going to return number of days left until the certificate expires instead of the expiry date")
 )
+
+func sslCheck(url string) (time.Time, error) {
+  conn, err := tls.Dial("tcp", fmt.Sprintf("%s:443", url), nil)
+  if err != nil {
+    return time.Time{}, errors.New("Couldn't establish a connection with the site. It's possible that you misspelled it's name or it has no certificate")
+  }
+  err = conn.VerifyHostname(url)
+  if err != nil {
+    return time.Time{}, errors.New("Couldn't find ssl certificate on the site.")
+  }
+  expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+  return expiry, nil
+}
 
 func main() {
 
   flag.Parse()
-  if *filename == "" && *email == ""{
-  } else {
-    fmt.Println("Provided filename:", *filename)  
-    fmt.Println("Provided email:", *email)  
+  if *domain != "" && *filename != "" {
+    fmt.Println("You can't choose both -d and -f flag at the same time.")
+    os.Exit(2)
+  } else if *domain != "" {
+    func(domain string, left bool) {
+      ex, err := sslCheck(domain) 
+      if err != nil {
+        fmt.Printf("%s: %s\n", domain, err)
+        os.Exit(2)
+      } else {
+        if left {
+          left := ex.Sub(time.Now()) 
+          fmt.Printf("%s: %s | %d days left\n", domain, ex, int(left.Hours()) / 24)
+        } else {
+          fmt.Printf("%s: %s\n", domain, ex)
+        }
+      }
+    }(*domain, *left)
+  } else if *domain == "" && *filename == "" {
+    fmt.Println("You have to provide a single domain (-d flag) or a file that contain domains (-f flag)")
+  } else if *filename != "" {
+    fmt.Println("Reading from file:", *filename) 
   }
 }

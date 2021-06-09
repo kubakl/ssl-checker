46a041ca8c33b2cd0b60075e6a51d82192a9dea0
Jakub Klimek <kuba@edp.pl>
2021-06-09 19:38:55 +0200

    Add an option to read domains from a file

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
   "time"
   "crypto/tls"  
   "errors"
+  "bufio"
+  "sync"
 )
 
 var (
@@ -30,8 +32,9 @@ func sslCheck(url string) (time.Time, error) {
 }
 
 func main() {
-
+  var wg sync.WaitGroup
   flag.Parse()
+
   if *domain != "" && *filename != "" {
     fmt.Println("You can't choose both -d and -f flag at the same time.")
     os.Exit(2)
@@ -54,5 +57,34 @@ func main() {
     fmt.Println("You have to provide a single domain (-d flag) or a file that contain domains (-f flag)")
   } else if *filename != "" {
     fmt.Println("Reading from file:", *filename) 
+    readFile, err := os.Open(*filename)
+    if err != nil {
+      fmt.Println("Wrong filename was provided.")
+    }
+    fileScanner := bufio.NewScanner(readFile)
+    fileScanner.Split(bufio.ScanLines)
+    var lines []string
+    for fileScanner.Scan() {
+      lines = append(lines, fileScanner.Text())
+    }
+    readFile.Close()
+    wg.Add(len(lines))
+    for _, line := range lines {
+      go func(domain string, left bool) {
+        defer wg.Done() 
+        ex, err := sslCheck(domain) 
+        if err != nil {
+          fmt.Printf("%s: %s\n", domain, err)
+        } else {
+          if left {
+            left := ex.Sub(time.Now()) 
+            fmt.Printf("%s: %s | %d days left\n", domain, ex, int(left.Hours()) / 24)
+          } else {
+            fmt.Printf("%s: %s\n", domain, ex)
+          }
+        }
+      }(line, *left)
+    }
+    wg.Wait()
   }
 }
